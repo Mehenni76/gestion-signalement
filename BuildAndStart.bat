74b505da3911241e5a79c84a0bab0fdeeb5d9b71
mehenni76 <djaddamehenni@gmail.comclear>
2021-07-31 13:48:49 +0200

    quelques modif

--- BuildAndStart.bat
+++ BuildAndStart.bat
@@ -1,5 +1,5 @@
 cd /d "%~dp0"
 REM gestion-signalement
 call mvn clean install -f pom.xml
- java -jar ./target/gestion-signalement-0.0.1-SNAPSHOT.jar
+ java -jar ./target/gestion-signalement.jar
 pause
--- src/main/java/fr/eql/aec/GestionSignalementApplication.java
+++ src/main/java/fr/eql/aec/GestionSignalementApplication.java
@@ -4,9 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-@EnableTransactionManagement
 @SpringBootApplication
 public class GestionSignalementApplication extends SpringBootServletInitializer{
 	public static void main(String[] args) {
